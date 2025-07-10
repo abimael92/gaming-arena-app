25f8fdae8d157b7ff243fc61fd7cc03612896fca
Abimael <abimael1992g@gmail.com>
2025-07-10 13:46:55 -0600

    chore: add API user.js for user management endpoints

---
+++ ecommerce-client/src/api/user.js
@@ -0,0 +1,106 @@
+import { ENV, authFetch } from "@/utils";
+
+export class User {
+  async getMe() {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.USERS_ME}`;
+
+      const response = await authFetch(url);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllUsers() {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.USERS}`;
+      const response = await fetch(url);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateMe(userId, data) {
+    try {
+      const url = `${ENV.API_URL} /${ENV.ENDPOINTS.USERS}/${userId} `;
+
+      const params = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createUser(data) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.USERS}`;
+
+      const params = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error(JSON.stringify(result.error)); // Ensure error object contains details
+      }
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateUser(userId, data) {
+    try {
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.USERS}/${userId}`;
+
+      const params = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await authFetch(url, params);
+      const result = await response.json();
+
+      if (response.status !== 200) {
+        throw new Error(getErrorMessage(result));
+      }
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
