c75e52f2b83d147631bbf0d9551d3a67bd1d6aff
Abimael <abimael1992g@gmail.com>
2025-07-19 23:05:20 -0600

    Update Account component index

---
+++ ecommerce-client/src/components/Account/index.js
@@ -0,0 +1,7 @@
+export * from "./Info";
+export * from "./Settings";
+export * from "./Address";
+export * from "./Game";
+export * from "./User";
+export * from "./Wishlist";
+export * from "./Orders";
