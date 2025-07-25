cb50d40247d3a34282a0869fd52c931bc382acdb
Abimael <abimael1992g@gmail.com>
2025-07-25 15:18:14 -0600

    ✨ Add BarTrust data configuration

---
+++ ecommerce-client/src/components/Shared/BarTrust/BarTrust.data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    icon: 'truck',
+    title: 'Super Fast',
+    description: 'Delivery in 24/48 hours',
+  },
+  {
+    icon: 'shield',
+    title: 'Reliable and Secure',
+    description: 'Over 10,000 games',
+  },
+  {
+    icon: 'wechat',
+    title: 'Customer Support',
+    description: 'Agent available 24/7',
+  },
+];
