3ea5aca16e05451ef5d0517e03562170cfd085b7
Abimael Garcia <abimael1992g@gmail.com>
2025-10-21 11:23:20 -0600

    feat: add conditional close button to HeroLayout

--- ecommerce-client/src/layouts/HeroLayout/HeroLayout.js
+++ ecommerce-client/src/layouts/HeroLayout/HeroLayout.js
@@ -1,32 +1,40 @@
 import Link from 'next/link';
 import { Icon } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+
 import LogoTitle from '@/components/Layout/LogoTitle';
 import styles from './HeroLayout.module.scss';
 
 export function HeroLayout(props) {
   const { children } = props;
+  const router = useRouter();
 
   return (
-    <>
+    <div className={styles.hero}>
+
+
       <div className={styles.topBar}>
         <LogoTitle size="MEDIUM" />
-        <Link href="/" className={styles.closeButton}>
-          <Icon name="close" />
-        </Link>
+        {router.pathname !== '/' && (
+          <Link href="/" className={styles.closeButton}>
+            <Icon name="close" />
+          </Link>
+        )}
       </div>
 
-      <div className={styles.container}>
+      {/* <div className={styles.container}> */}
 
 
-        <div className={styles.content}>
-          <div className={styles.blockLeft}>
-            {children}
-          </div>
-
-          <div className={styles.blockRight} />
+      <div className={styles.content}>
+        <div className={styles.blockLeft}>
+          {children}
         </div>
+
+        <div className={styles.blockRight} />
       </div>
-    </>
+      {/* </div> */}
+    </div >
+
 
   );
 }
